de93755f49db0a8de68df03e293db7d32c235985
Joon <ddoonngg2@naver.com>
2023-05-12 15:41:51 +0900

    나누기 연산

---
+++ src/main/java/kr/ac/sahmyook/Application.java
@@ -0,0 +1,17 @@
+package kr.ac.sahmyook;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("정수 두개를 입력해주세요 :");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        System.out.println("계산 결과 " + (num1 / num2));
+
+    }
+}
